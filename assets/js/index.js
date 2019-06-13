5ba5150fc1c082bddd2d1df4e9041bdf90436126
Aaron Keisler <amkeisler@gmail.com>
2019-06-13 10:05:59 -0400

    update fetch request

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,12 +9,6 @@ document.addEventListener("submit", function(e) {
   renderBtns();
 });
 
-/*
- `q`
- `limit`
- `rating`
-*/
-
 function renderBtns() {
   while (document.querySelector("#buttons").firstChild) {
     document
@@ -29,38 +23,86 @@ function renderBtns() {
     document.querySelector("#buttons").append(el);
   });
 }
+// send the right headers
+// correct content-type
+// legal 'access-control-allow-origin' on the response
 
 function getGifs() {
-  queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${
+  const queryURL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${
     this.dataset.name
   }&limit=5&offset=0&lang=en`;
-  fetch(queryURL, { credentials: "include", mode: "cors" })
+  fetch(queryURL, { mode: "cors" })
     .then(data => data.json())
     .then(data => {
-      console.log(data);
-      results = data.data;
+      if (data.data !== undefined) {
+        console.log(data);
+        results = data.data;
+        results.forEach(item => {
+          // create elements
+          const elTitle = document.createElement("h4");
+          const elImg = document.createElement("img");
+          const elRating = document.createElement("p");
+          const newDiv = document.createElement("div");
+
+          // grab data from response
+          const title = item.title.replace(/ GIF.*/g, "");
+          const imgSrc = item.embed_url;
+          const rating = item.rating;
+
+          // append text and place in div
+          elTitle.appendChild(document.createTextNode(title));
+          elRating.appendChild(document.createTextNode(rating));
+          elImg.src = imgSrc;
+          newDiv.append(elImg);
+          newDiv.append(elTitle);
+          newDiv.append(elRating);
+
+          // append to document
+          document.querySelector("#targetDiv").append(newDiv);
+        });
+        // if undefined response, throw error
+      } else {
+        throw new Error("data returned as undefined, check what was entered");
+      }
+    })
+    //catch errors
+    .catch(err => console.error(err));
+}
+
+function getGifsTenor() {
+  $("button").on("click", function() {
+    var queryURL =
+      "https://api.tenor.com/v1/search?=" +
+      this.dataset.name +
+      "&key=LIVDSRZULELA&limit=10";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      let results = response.results;
+      console.log(results);
+
       results.forEach(item => {
         // create elements
         const elTitle = document.createElement("h4");
+        elTitle.appendChild(
+          document.createTextNode(item.title.replace(/ GIF.*/g, ""))
+        );
         const elImg = document.createElement("img");
+        elImg.src = item.url;
         const elRating = document.createElement("p");
+        elRating.appendChild(document.createTextNode(item.rating));
         const newDiv = document.createElement("div");
 
-        // grab data from response
-        const title = item.title.replace(/ GIF.*/g, "");
-        const imgSrc = item.url;
-        const rating = item.rating;
-
         // append text and place in div
-        elTitle.appendChild(document.createTextNode(title));
-        elRating.appendChild(document.createTextNode(rating));
-        elImg.src = imgSrc;
         newDiv.append(elImg);
         newDiv.append(elTitle);
         newDiv.append(elRating);
 
         // append to document
-        document.querySelector("#targetDiv").append(newDiv);
+        document.querySelector("#targetDiv").prepend(newDiv);
       });
     });
+  });
 }
