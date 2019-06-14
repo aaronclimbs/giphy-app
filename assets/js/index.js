ba6675400a2b0e5787000b1cfbf64ea6924fd46d
Aaron Keisler <amkeisler@gmail.com>
2019-06-14 19:18:39 -0400

    add clear storage and separate render function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,3 @@
-// TO DO
-// Add clear local storage functionality
-// Fix save and clear save button padding
-// Turn rendering GIFs into a separate function that can be reused for viewing saved gifs
-
 const API_KEY = "29kNYcuu8j6tch4gMNKSN5VQQufCvlGp";
 const topics = ["rock climbing", "baseball", "tennis", "basketball"];
 let viewSaved = false;
@@ -12,15 +7,22 @@ document.querySelector("#addSport").addEventListener("submit", function(e) {
   e.preventDefault();
   console.log(e);
   input = e.target.elements.input;
-  if (topics.indexOf(input.value.toLowerCase()) === -1) {
-    topics.push(input.value.toLowerCase());
+  if (input.value === null || input.value === "") {
+    alert("Please enter a search term.");
   } else {
-    alert("This button's already here.");
+    if (input.value.replace(/\d/g, "").length === 0) {
+      alert("Please enter a word.");
+    } else if (topics.indexOf(input.value.toLowerCase()) === -1) {
+      topics.push(input.value.toLowerCase());
+    } else {
+      alert("This button's already here.");
+    }
   }
   input.value = "";
   renderBtns();
 });
 
+document.querySelector("#clearSaves").addEventListener("click", clearSaves);
 document.querySelector("#viewSaves").addEventListener("click", viewSaves);
 
 function renderBtns() {
@@ -36,23 +38,31 @@ function renderBtns() {
 }
 
 function viewSaves() {
-  viewSaved = true;
-  clearOutByID("targetDiv");
-  const saved = localStorage.getItem("favGifs").split(",");
-  saved.forEach(id => {
-    const queryURLSaved = `https://api.giphy.com/v1/gifs/${id}?api_key=${API_KEY}`;
-    fetch(queryURLSaved, { mode: "cors" })
-      .then(data => data.json())
-      .then(data => {
-        if (data.data !== undefined) {
-          console.log(data.data);
-        } else {
-          throw new Error("data returned as undefined, check what was entered");
-        }
-      })
-      //catch errors
-      .catch(err => console.error(err));
-  });
+  if (localStorage.getItem("favGifs").length === 0) {
+    alert("Save some items first!");
+  } else {
+    viewSaved = true;
+    clearOutByID("targetDiv");
+    const saved = localStorage.getItem("favGifs").split(",");
+    saved.forEach(id => {
+      const queryURLSaved = `https://api.giphy.com/v1/gifs/${id}?api_key=${API_KEY}`;
+      fetch(queryURLSaved, { mode: "cors" })
+        .then(data => data.json())
+        .then(data => {
+          if (data.data !== undefined) {
+            item = data.data;
+            renderGifs(item);
+            // if undefined response, throw error
+          } else {
+            throw new Error(
+              "data returned as undefined, check what was entered"
+            );
+          }
+        })
+        //catch errors
+        .catch(err => console.error(err));
+    });
+  }
 }
 
 function getGifs() {
@@ -68,69 +78,7 @@ function getGifs() {
       if (data.data !== undefined) {
         console.log(data.data);
         results = data.data;
-        results.forEach(item => {
-          // create elements
-          const elImg = document.createElement("img");
-          const elRating = document.createElement("p");
-          const newDiv = document.createElement("div");
-          const dLoad = document.createElement("span");
-          const save = document.createElement("span");
-
-          // grab data from response
-          const id = item.id;
-          const animated = item.images.fixed_width.url;
-          const still = item.images.fixed_width_still.url;
-          const rating = item.rating;
-
-          // append text and place in div
-          dLoad.className = "iconDownload";
-          dLoad.innerHTML = `<a href=${
-            item.images.downsized_medium.url
-          } download=${replaceSpaces(
-            item.title
-          )} target="_blank"><i class='fas fa-external-link-square-alt'/></a>`;
-          save.className = "iconSave";
-          save.innerHTML = `<i class="far fa-heart" data-save="unsaved"></i>`;
-          save.addEventListener("click", e => {
-            let btn = e.target;
-            let gifID = btn.parentElement.parentElement.firstChild.dataset.id;
-            if (btn.getAttribute("data-save") == "unsaved") {
-              btn.classList.remove("far");
-              btn.classList.add("fas");
-              btn.setAttribute("data-save", "saved");
-              addToLocal("favGifs", gifID);
-            } else {
-              btn.classList.remove("fas");
-              btn.classList.add("far");
-              btn.setAttribute("data-save", "unsaved");
-              removeFromLocal("favGifs", gifID);
-            }
-          });
-          elRating.appendChild(document.createTextNode(`Rated: ${rating}`));
-          elRating.style.textAlign = "center";
-          elRating.className = "animated zoomInLeft";
-          elImg.src = still;
-          elImg.setAttribute("data-id", id);
-          elImg.setAttribute("data-state", "still");
-          elImg.className = "animated flipInY";
-          elImg.addEventListener("click", e => {
-            let gif = e.target;
-            if (gif.getAttribute("data-state") === "still") {
-              gif.src = animated;
-              gif.setAttribute("data-state", "animate");
-            } else {
-              gif.src = still;
-              gif.setAttribute("data-state", "still");
-            }
-          });
-          newDiv.className = "imgContainer";
-          newDiv.append(elImg);
-          newDiv.append(elRating);
-          newDiv.append(dLoad);
-          newDiv.append(save);
-          // append to document
-          document.querySelector("#targetDiv").prepend(newDiv);
-        });
+        results.forEach(item => renderGifs(item));
         // if undefined response, throw error
       } else {
         throw new Error("data returned as undefined, check what was entered");
@@ -167,4 +115,77 @@ function clearOutByID(target) {
   while (targetDiv.firstChild) {
     targetDiv.removeChild(targetDiv.firstChild);
   }
+}
+
+function renderGifs(item) {
+  const elImg = document.createElement("img");
+  const elRating = document.createElement("p");
+  const newDiv = document.createElement("div");
+  const dLoad = document.createElement("span");
+  const save = document.createElement("span");
+
+  // grab data from response
+  const id = item.id;
+  const animated = item.images.fixed_width.url;
+  const still = item.images.fixed_width_still.url;
+  const rating = item.rating;
+
+  // append text and place in div
+  dLoad.className = "iconDownload";
+  dLoad.innerHTML = `<a href=${
+    item.images.downsized_medium.url
+  } download=${replaceSpaces(
+    item.title
+  )} target="_blank"><i class='fas fa-external-link-square-alt'/></a>`;
+  save.className = "iconSave";
+  if (localStorage.getItem("favGifs").includes(id)) {
+    save.innerHTML = `<i class="fas fa-heart" data-save="saved"></i>`;
+  } else {
+    save.innerHTML = `<i class="far fa-heart" data-save="unsaved"></i>`;
+  }
+  save.addEventListener("click", e => {
+    let btn = e.target;
+    let gifID = btn.parentElement.parentElement.firstChild.dataset.id;
+    if (btn.getAttribute("data-save") === "unsaved") {
+      btn.classList.remove("far");
+      btn.classList.add("fas");
+      btn.setAttribute("data-save", "saved");
+      addToLocal("favGifs", gifID);
+    } else {
+      btn.classList.remove("fas");
+      btn.classList.add("far");
+      btn.setAttribute("data-save", "unsaved");
+      removeFromLocal("favGifs", gifID);
+    }
+  });
+  elRating.appendChild(document.createTextNode(`Rated: ${rating}`));
+  elRating.style.textAlign = "center";
+  elRating.className = "animated zoomInLeft";
+  elImg.src = still;
+  elImg.setAttribute("data-id", id);
+  elImg.setAttribute("data-state", "still");
+  elImg.className = "animated flipInY";
+  elImg.addEventListener("click", e => {
+    let gif = e.target;
+    if (gif.getAttribute("data-state") === "still") {
+      gif.src = animated;
+      gif.setAttribute("data-state", "animate");
+    } else {
+      gif.src = still;
+      gif.setAttribute("data-state", "still");
+    }
+  });
+  newDiv.className = "imgContainer";
+  newDiv.append(elImg);
+  newDiv.append(elRating);
+  newDiv.append(dLoad);
+  newDiv.append(save);
+  // append to document
+  document.querySelector("#targetDiv").prepend(newDiv);
+}
+
+function clearSaves() {
+  localStorage.setItem("favGifs", "");
+  alert("Local storage cleared.");
+  document.location.reload();
 }
